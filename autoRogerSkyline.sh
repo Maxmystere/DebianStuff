cb2ff07be41aae4e4df8fd6a1923b59033db63c1
Maxence GRAB <magrab@e2r11p14.42.fr>
2019-02-19 22:42:50 +0100

    Correction false error

--- autoRogerSkyline.sh
+++ autoRogerSkyline.sh
@@ -1,6 +1,6 @@
 if ! command -v sudo 1>/dev/null; then
-	if [ "$EUID" -ne 0 ]
-	then echo "Please run as root"
+	if [ "$EUID" -ne 0 ]; then
+		echo "Please run as root"
 		exit
 	fi
 	echo "Ready to go"
