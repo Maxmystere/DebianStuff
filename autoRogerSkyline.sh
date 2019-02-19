c8456803eef3aea7905b0fbe8e54e2c904001645
Maxence GRAB <magrab@e2r11p14.42.fr>
2019-02-20 00:03:06 +0100

    Add installation for sudo

--- autoRogerSkyline.sh
+++ autoRogerSkyline.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 if ! command -v sudo 1>/dev/null; then
 	if [ "$EUID" -ne 0 ]; then
-		echo "Please run as root"
+		echo "Please run as root (su) to install sudo"
 		exit
 	fi
-	echo "Ready to go"
+	echo "Ready to go\nUpdating Everything"
+	apt update
+	apt upgrade
+	echo "Installing sudo and disconnecting user"
+	apt install sudo
+	sleep 1
+	adduser max sudo
+	echo "SUCCESS ! You will now be disconnected"
+	sleep 1
+	pkill -KILL -u max
+	exit
 else
-	echo "Good"
+	if ! [ "$EUID" -ne 0 ]; then
+		echo "Please do not run as root"
+		exit
+	else
+		echo "Working !"
+	fi
 fi
