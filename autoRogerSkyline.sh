669d86cf7d781a46269961a012d5c0fb5b3a0b04
Maxence GRAB <magrab@e1r13p8.42.fr>
2019-02-20 15:01:36 +0100

    Update

--- autoRogerSkyline.sh
+++ autoRogerSkyline.sh
@@ -17,9 +17,24 @@ if ! command -v sudo 1>/dev/null; then
 	exit
 else
 	if ! [ "$EUID" -ne 0 ]; then
-		echo "Please do not run as root"
+		echo "Please do not run as root (Sudo is installed)"
 		exit
 	else
-		echo "Working !"
+
+		echo "Ready to go ! Updating Apt"
+		sudo apt update
+		sudo apt upgrade
+		echo "Installing vim"
+		sudo apt install vim -y
+		echo "We will change DHCP Last due to Bridged Adapter (For 42)"
+		sleep 4
+		echo "Installing ssh"
+		sudo apt install ssh -y
+		echo "We will now change ssh port  to 4269"
+		sleep 3
+		sudo sed -i 's/#Port 22/Port 4269/' /etc/ssh/sshd_config
+		echo "Done ! Restarting SSH"
+		sudo service sshd restart
+
 	fi
 fi
