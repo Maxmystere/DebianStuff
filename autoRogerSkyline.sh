1f4a39e07cf37c553951dd5ce644018c0a09ccdb
Maxence GRAB <magrab@e1r13p8.42.fr>
2019-02-21 02:25:39 +0100

    Secure color

--- autoRogerSkyline.sh
+++ autoRogerSkyline.sh
@@ -1,50 +1,50 @@
 #!/bin/bash
 if ! command -v sudo 1>/dev/null; then
 	if [ "$EUID" -ne 0 ]; then
-		echo "\e[31mPlease run as root (su) to install sudo"
+		echo -e "\e[31mPlease run as root (su) to install sudo\e[0m"
 		exit
 	fi
-	echo "\e[33mReady to go ! Updating Everything"
+	echo -e "\e[33mReady to go ! Updating Everything\e[0m"
 	apt update
 	apt upgrade
-	echo "\e[33mInstalling sudo and disconnecting user"
+	echo -e "\e[33mInstalling sudo and disconnecting user\e[0m"
 	apt install sudo
 	#sleep 1
 	adduser max sudo
-	echo "\e[33mSUCCESS ! You will now be disconnected"
+	echo -e "\e[33mSUCCESS ! You will now be disconnected\e[0m"
 	#sleep 2
 	pkill -KILL -u max
 	exit
 else
 	if ! [ "$EUID" -ne 0 ]; then
-		echo "\e[31mPlease do not run as root (Sudo is installed)"
+		echo -e "\e[31mPlease do not run as root (Sudo is installed)\e[0m"
 		exit
 	else
 
-		echo "\e[33mReady to go ! Updating Apt"
+		echo -e "\e[33mReady to go ! Updating Apt\e[0m"
 		sudo apt update
 		sudo apt upgrade
-		echo "\e[33mInstalling vim"
+		echo -e "\e[33mInstalling vim\e[0m"
 		sudo apt install vim -y
-		echo "\e[33mWe will change DHCP Last due to Bridged Adapter (For 42)"
+		echo -e "\e[33mWe will change DHCP Last due to Bridged Adapter (For 42)\e[0m"
 		#sleep 4
 
-		echo "\e[33mInstalling ssh"
+		echo -e "\e[33mInstalling ssh\e[0m"
 		sudo apt install ssh -y
-		echo "\e[33mWe will now :"
-		echo "\e[33mchange ssh port to 4269"
-		echo "\e[33mallow only public key"
-		echo "\e[33mPrevent root access from ssh"
+		echo -e "\e[33mWe will now :"
+		echo -e "\e[33mchange ssh port to 4269\e[0m"
+		echo -e "\e[33mallow only public key\e[0m"
+		echo -e "\e[33mPrevent root access from ssh\e[0m"
 		#sleep 3
 
 		sudo sed -i 's/#Port 22/Port 4269/' /etc/ssh/sshd_config
 		sudo sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
 		sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin no/' /etc/ssh/sshd_config
-		echo "\e[33mDone ! Restarting SSH"
+		echo -e "\e[33mDone ! Restarting SSH\e[0m"
 		sudo service sshd restart
-		echo "\e[33mSuccess"
+		echo -e "\e[33mSuccess\e[0m"
 		
-		echo "\e[33mInstalling firewall"
+		echo -e "\e[33mInstalling firewall\e[0m"
 		#sleep 1
 		printf "#!/bin/sh
 # /etc/init.d/firewall
@@ -123,10 +123,10 @@ exit 0
 		sudo chmod +x /etc/init.d/firewall
 		sudo update-rc.d firewall defaults
 		sudo sh /etc/init.d/firewall
-		echo "\e[33mFirewall and DDOS Protection successfully installed at startup"
+		echo -e "\e[33mFirewall and DDOS Protection successfully installed at startup\e[0m"
 		#sleep 2
 
-		echo "\e[33mAdding planned task for updating apt"
+		echo -e "\e[33mAdding planned task for updating apt\e[0m"
 		#sleep 1
 		printf "#!/bin/sh
 
@@ -135,14 +135,14 @@ apt upgrade
 " | sudo tee /etc/init.d/update_package.sh
 		sudo chmod +x /etc/init.d/update_package.sh
 		sudo update-rc.d update_package.sh defaults
-		echo "\e[33m00 04 * * 1 /etc/init.d/update_package.sh >> /var/log/update_script.log" | sudo crontab -
-		echo "\e[33mDone"
+		echo "00 04 * * 1 /etc/init.d/update_package.sh >> /var/log/update_script.log" | sudo crontab -
+		echo -e "\e[33mDone\e[0m"
 		#sleep 1
 
-		echo "\e[33mInstalling incron and mailutils to spy /etc/crontab"
+		echo -e "\e[33mInstalling incron and mailutils to spy /etc/crontab\e[0m"
 		sudo apt install mailutils -y
 		sudo apt install incron -y
-		echo "\e[33mroot" | sudo tee /etc/incron.allow
+		echo "root" | sudo tee /etc/incron.allow
 		printf "/etc/crontab IN_MODIFY echo \"crontab file has been modified\" | mail -s \"crontab Alert\" root@localhost\n" | sudo incrontab -
 	fi
 fi
